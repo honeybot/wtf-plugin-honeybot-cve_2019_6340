1acc3afb86753c5b6dd49b129006944fd0db289f
Alexey Goncharov <alyoshapotter@gmail.com>
2019-03-06 17:56:06 +0300

    Fixed bug in handler

--- wtf/plugins/honeybot/cve_2019_6340/handler.lua
+++ wtf/plugins/honeybot/cve_2019_6340/handler.lua
@@ -7,13 +7,10 @@ _M.name = "honeybot.cve_2019_6340"
 
 function _M:content(...)
   local ngx = ngx
-  local pairs = pairs
   local select = select
-  local err = tools.error
   local instance = select(1, ...)
-  local string = string
-  local die
-  local payload = {"message":"Type http:\/\/127.0.0.1\/rest\/type\/node\/INVALID_VALUE does not correspond to an entity on this site."}
+  local payload = {}
+  payload["message"] = "Type http://127.0.0.1/rest/type/node/INVALID_VALUE does not correspond to an entity on this site."
   
   local do_action = self:get_mandatory_parameter('action')
   instance:get_action(do_action):act(payload)
